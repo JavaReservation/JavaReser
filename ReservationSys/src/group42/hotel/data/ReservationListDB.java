486d474430cc8e19d3a37d9c69a446c10aacc243
thatNinja42 <josepepin2@gmail.com>
2016-11-11 12:08:01 -0800

    starting resvationDB allover again

--- ReservationSys/src/group42/hotel/data/ReservationListDB.java
+++ ReservationSys/src/group42/hotel/data/ReservationListDB.java
@@ -1,5 +1,21 @@
 package group42.hotel.data;
 
+import java.util.List;
+
+import dw317.hotel.business.interfaces.HotelFactory;
+import dw317.hotel.business.interfaces.Reservation;
+import dw317.hotel.business.interfaces.Room;
+import dw317.hotel.data.interfaces.ListPersistenceObject;
+
 public class ReservationListDB {
+
+	private List<Reservation> database;
+	private List<Room> allRooms;
+	private final ListPersistenceObject listPersistenceObject;
+	private final HotelFactory factory;
+
+	public ReservationListDB (ListPersistenceObject listPersistenceObject){
+		
+	}
 
 }
