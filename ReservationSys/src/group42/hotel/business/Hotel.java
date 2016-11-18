3e6b616745d531a496ed7615e5ad30a9f4a9871e
Keylen <Keeson@hotmail.com>
2016-11-18 10:31:24 -0500

    Part 3 is finished, working on part 4
    Started part 4

---
+++ ReservationSys/src/group42/hotel/business/Hotel.java
@@ -0,0 +1,13 @@
+package group42.hotel.business;
+
+public class Hotel {
+
+	private final HotelFactory factory;
+	private final CustomerDAO customers;
+	private final ReservationDAO reservations;
+	private static final long serialVersionUID = 42031768871L;
+
+	public Hotel (HotelFactory factory, RoomDAO rooms, CustomerDAO customers,
+			ReservationDAO reservations
+
+}
