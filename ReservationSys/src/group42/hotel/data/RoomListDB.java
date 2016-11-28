75df2876b6887eb1ba53e52fa2433864fcfcfff3
Keylen <Keeson@hotmail.com>
2016-11-28 17:21:00 -0500

    Revert "commiting so werner can fix this shiz"
    This reverts commit 2751b9e4d715e98772738ac7e287419212e0539f.

--- ReservationSys/src/group42/hotel/data/RoomListDB.java
+++ ReservationSys/src/group42/hotel/data/RoomListDB.java
@@ -17,7 +17,7 @@ import dw317.hotel.data.interfaces.*;
 public class RoomListDB implements RoomDAO {
 
 	private List<Room> database;
-	private final ObjectSerializedList objectSerializedList;
+	private final ListPersistenceObject listPersistenceObject;
 		
 	/**
 	 * Constructor requires a ListPersistenceObject to read from file.
@@ -26,9 +26,9 @@ public class RoomListDB implements RoomDAO {
 	 * @throws ClassNotFoundException 
 	 * @throws FileNotFoundException 
 	 */
-	public RoomListDB (ObjectSerializedList objectSerializedList ) throws FileNotFoundException, ClassNotFoundException, IOException {
-		this.objectSerializedList = objectSerializedList ;
-		this.database = this.objectSerializedList .getRoomDatabase();   
+	public RoomListDB (ListPersistenceObject listPersistenceObject) throws FileNotFoundException, ClassNotFoundException, IOException {
+		this.listPersistenceObject = listPersistenceObject;
+		this.database = this.listPersistenceObject.getRoomDatabase();   
 		
 	}
 
